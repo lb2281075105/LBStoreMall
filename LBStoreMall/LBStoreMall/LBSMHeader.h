969ec4dfb6aa93ae9c11e6f380f19fa282cfda16
CodeJCSON <2334497007@qq.com>
2017-09-22 09:36:25 +0800

    导航栏设置

--- LBStoreMall/LBStoreMall/LBSMHeader.h
+++ LBStoreMall/LBStoreMall/LBSMHeader.h
@@ -15,11 +15,10 @@
 ///如果是发布状态LBNCLog就直接为空
 #define kHeight [UIScreen cz_screenHeight] / 750.0
 //标题字体
-#define kTitleFont      [UIFont systemFontOfSize:15]
-//子标题字体
-#define kSubtitleFont      [UIFont systemFontOfSize:13]
+#define LBSMFont(font)      [UIFont systemFontOfSize:font]
+
 //通过RGB设置颜色
-#define kRGBColor(R,G,B)        [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1.0]
+#define LBSMColor(R,G,B)        [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1.0]
 //应用程序的屏幕高度
 #define kWindowH        [UIScreen mainScreen].bounds.size.height
 //应用程序的屏幕宽度
--- LBStoreMall/LBStoreMall/classes/controller/main/LBSMNavController.m
+++ LBStoreMall/LBStoreMall/classes/controller/main/LBSMNavController.m
@@ -14,9 +14,46 @@
 
 @implementation LBSMNavController
 
+/// 只走一次
++ (void)initialize{
+
+    UINavigationBar *bar = [UINavigationBar appearance];
+    bar.barTintColor = LBSMColor(245,245,245);
+    [bar setShadowImage:[UIImage new]];
+    [bar setTintColor:[UIColor clearColor]];
+    /// 设置导航栏字体颜色
+    bar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor blackColor],NSFontAttributeName:LBSMFont(18)};
+
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    /// 手势返回
+    [GQGesVCTransition validateGesBackWithType:GQGesVCTransitionTypePanWithPercentRight withRequestFailToLoopScrollView:YES];
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    LBSMLog(@"%ld",self.childViewControllers.count);
+    if (self.childViewControllers.count >= 1) {
+        /// 返回按钮自定义
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"navigationbar_back"] style:UIBarButtonItemStyleDone target:self action:@selector(navBackClick)];
+        /// 影藏BottomBar
+        viewController.hidesBottomBarWhenPushed = YES;
+    }
+    /// 跳转
+    [super pushViewController:viewController animated:animated];
+}
+/// 导航返回
+- (void)navBackClick{
+    [self popViewControllerAnimated:YES];
+}
+
+//- (UIStatusBarStyle)preferredStatusBarStyle
+//{
+//    return UIStatusBarStyleLightContent;
+//}
+
+- (UIViewController *)childViewControllerForStatusBarStyle{
+    return self.topViewController;
 }
 
 @end
